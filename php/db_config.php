9645c28e45f024317e1c91e543ac67fba4efe6b9
gabrieltimisica <37377790+gabrieltimisica@users.noreply.github.com>
2018-07-25 09:40:21 +0300

    Delete db_config.php

--- php/db_config.php
+++
@@ -1,8 +0,0 @@
-<?php
-//    define('DB_SERVER', '192.168.15.47:3306');
-   define('DB_SERVER', '192.168.12.106:3306');
-   define('DB_USERNAME', 'romprix');
-   define('DB_PASSWORD', 'romprix');
-   define('DB_DATABASE', 'test');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-?>
